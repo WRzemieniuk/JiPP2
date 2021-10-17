6d2a9c9aace03eef22f215e1cfed16453ebba7d9
Twoje Imię Nazwisko <toja@example.com>
2021-10-18 00:57:31 +0200

    First commit Simple Calculator

---
+++ include/calc.h
@@ -0,0 +1,7 @@
+
+int add(int a, int b);
+int subtract(int a, int b);
+double volume(int a, int b, int h, int H);
+void help();
+void menu(int argc, char *argv[]);
+void error(int argc, char *argv[]);
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "../include/calc.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    help();
+    menu(argc, argv);
+    return 0;
+}
